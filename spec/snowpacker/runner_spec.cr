0885930f8ce346631a33ce996993d0840b61f52a
Andrew <awcrotwell@gmail.com>
2021-01-24 17:56:15 -0500

    CI

--- spec/snowpacker/runner_spec.cr
+++ spec/snowpacker/runner_spec.cr
@@ -8,6 +8,7 @@ module Snowpacker
         config.enabled = true
       end
 
+      sleep 2 # Wait for any process to die
       begin
         process = Engine.run
         process.should_not be_nil
@@ -25,7 +26,7 @@ module Snowpacker
       begin
         sleep 2 # Wait for any process to die
         process = Engine.run
-        sleep 3 # Wait for the process to turn on
+        sleep 2 # Wait for the process to turn on
         process2 = Engine.run
 
         process.should_not be_nil
--- spec/snowpacker/utils_spec.cr
+++ spec/snowpacker/utils_spec.cr
@@ -3,23 +3,22 @@ require "http/server"
 
 module Snowpacker
   describe Utils do
-    pending "can detect if a port is in use"
-    # it "can detect if a port is in use" do
-    #   Engine.configure do |config|
-    #     config.config_path = "spec"
-    #     config.enabled = true
-    #   end
+    it "can detect if a port is in use" do
+      Engine.configure do |config|
+        config.config_path = "spec"
+        config.enabled = true
+      end
 
-    #   begin
-    #     sleep 1 # Wait for any process to die
-    #     Utils.detect_port!.should be_true
-    #     process = Engine.run
-    #     sleep 2 # Wait for the process to turn on
+      begin
+        sleep 2 # Wait for any process to die
+        Utils.detect_port!.should be_true
+        process = Engine.run
+        sleep 2 # Wait for the process to turn on
 
-    #     Utils.detect_port!.should be_false
-    #   ensure
-    #     process.signal(Signal::KILL) if process
-    #   end
-    # end
+        Utils.detect_port!.should be_false
+      ensure
+        process.signal(Signal::KILL) if process
+      end
+    end
   end
 end
